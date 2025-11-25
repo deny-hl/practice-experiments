a592d6fc21dce11aaf7fcc46d0a6333693bcfca5
Denys <denys3xd@gmail.com>
2025-11-25 21:08:38 +0100

    practice with split method

---
+++ test.js
@@ -0,0 +1,7 @@
+// function to reverse words
+// let str = 'john McClane';
+// function nameShuffler(str) {
+//   return str.split(' ').reverse().join(' ');
+// }
+
+// console.log(nameShuffler(str));
