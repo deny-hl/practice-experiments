9b265928302748134fdcf364d1ce025ca9300771
Denys <denys3xd@gmail.com>
2025-12-21 10:41:58 +0100

    slice method

--- IndexOfmethod.js
+++ IndexOfmethod.js
@@ -10,3 +10,26 @@
 // numbers.indexOf(1, 3) === -1
 // numbers.indexOf(5, -2) === 5
 
+numbers.indexOf = function(searchElement, fromIndex) {
+  let start = this.length + fromIndex;
+
+  if (fromIndex === undefined) {
+    start = 0;
+  } else if (fromIndex >= 0) {
+    start = fromIndex;
+  } else {
+    start = this.length + fromIndex;
+
+    if (start < 0) {
+      start = 0;
+    };
+  }
+
+  for (let i = start; i < this.length; i++) {
+    if (this[i] === searchElement) {
+      return i;
+    }
+  };
+
+  return -1;
+};
